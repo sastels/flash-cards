45955c8d1a9c424bcc1b16d2353db962449344b0
Steve Astels <sastels@gmail.com>
2019-08-19 22:10:36 -0400

    refactor Home

--- src/Home.js
+++ src/Home.js
@@ -3,18 +3,24 @@ import { jsx } from '@emotion/core';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Box from '@material-ui/core/Box';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 import { areas } from './data/words';
 
 const useStyles = makeStyles(() => ({
   title: {
     flexGrow: 1
   },
-  selections: {
-    marginTop: '50px'
+  table: {
+    maxWidth: 350
+  },
+  tableCell: {
+    fontSize: '15pt'
   }
 }));
 
@@ -28,35 +34,24 @@ const Home = () => {
           <Typography variant="h6" className={classes.title}>
             Flash Cards
           </Typography>
-          <Button color="inherit" component={Link} to="/progress">
-            Progress
-          </Button>
         </Toolbar>
       </AppBar>
 
-      <Box
-        display="flex"
-        flexDirection="column"
-        marginTop="100px"
-        justifyContent="center"
-      >
-        {Object.keys(areas).map(area => (
-          <Box
-            key={area}
-            display="flex"
-            marginTop="30px"
-            justifyContent="center"
-          >
-            <Button
-              variant="contained"
-              color="primary"
-              component={Link}
-              to={`/flash-cards/${area}`}
-            >
-              {area}
-            </Button>
-          </Box>
-        ))}
+      <Box display="flex" marginTop="100px" justifyContent="center">
+        <Table className={classes.table}>
+          <TableBody>
+            {Object.keys(areas).map(area => (
+              <TableRow key={area}>
+                <TableCell className={classes.tableCell}>
+                  <Link to={`/flash-cards/${area}`}>{area}</Link>
+                </TableCell>
+                <TableCell className={classes.tableCell}>
+                  <Link to="/progress">Progress</Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </Box>
     </div>
   );
